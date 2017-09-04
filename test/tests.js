9eb13ebe9216b57ab991079326839e06f9277498
Petra Chocholova <petra.chocholova@gmail.com>
2017-09-04 17:49:21 +0200

    Refactoring

--- test/tests.js
+++ test/tests.js
@@ -15,48 +15,11 @@ describe('server response', function() {
     server.close();
   });
 
-  it('should return 400', function (done) {
-    request.get('http://localhost:4000', function (err, res, body){
-      expect(res.statusCode).to.equal(400);
-      expect(res.body).to.equal('wrong header');
-      done();
-    });
-  });
-
   it('should return 200', function (done) {
-    var options = {
-      url: 'http://localhost:4000',
-      headers: {
-        'Content-Type': 'text/plain'
-      }
-    };
-    request.get(options, function (err, res, body) {
+    request.get('http://localhost:4000', function (err, res, body) {
       expect(res.statusCode).to.equal(200);
-      expect(res.body).to.equal('correct header');
+      expect(res.body).to.equal('/');
       done();
     });
-  });
-
-  it('should emit request body', function(done) {
-    var options = {
-      url: 'http://localhost:4000',
-      headers: {
-        'Content-Type': 'text/plain'
-      },
-      body: 'successfully emitted request'
-    };
-    var eventFired = false;
-
-    request.get(options, function (err, res, body) {});
-
-    server.on('success', function (data) {
-      eventFired = true;
-      expect(data).to.equal('successfully emitted request');
-    });
-
-    setTimeout( function () {
-      expect(eventFired).to.equal(true);
-      done();
-    }, 10);
   });
 });
