48e570e1847cc486bdab52a3f1c44d1e1394e4f7
Petra Chocholova <petra.chocholova@gmail.com>
2017-09-04 16:34:14 +0200

    Server setup using Node.js

---
+++ test/tests.js
@@ -0,0 +1,62 @@
+'use strict';
+
+var expect = require('Chai').expect;
+
+var server = require('../lib/server.js');
+
+var request = require('request');
+
+describe('server response', function() {
+  before(function () {
+    server.listen(4000);
+  });
+
+  after(function () {
+    server.close();
+  });
+
+  it('should return 400', function (done) {
+    request.get('http://localhost:4000', function (err, res, body){
+      expect(res.statusCode).to.equal(400);
+      expect(res.body).to.equal('wrong header');
+      done();
+    });
+  });
+
+  it('should return 200', function (done) {
+    var options = {
+      url: 'http://localhost:4000',
+      headers: {
+        'Content-Type': 'text/plain'
+      }
+    };
+    request.get(options, function (err, res, body) {
+      expect(res.statusCode).to.equal(200);
+      expect(res.body).to.equal('correct header');
+      done();
+    });
+  });
+
+  it('should emit request body', function(done) {
+    var options = {
+      url: 'http://localhost:4000',
+      headers: {
+        'Content-Type': 'text/plain'
+      },
+      body: 'successfully emitted request'
+    };
+    var eventFired = false;
+
+    request.get(options, function (err, res, body) {});
+
+    server.on('success', function (data) {
+      eventFired = true;
+      expect(data).to.equal('successfully emitted request');
+    });
+
+    setTimeout( function () {
+      expect(eventFired).to.equal(true);
+      done();
+    }, 10);
+  });
+});
